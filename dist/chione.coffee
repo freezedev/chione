d8df6a45c254af658531006bf64be7131958a7b9
Johannes Stein <johannesstein@freeze-dev.com>
2013-11-25 22:55:39 +0100

    Added generated files

---
+++ dist/chione.coffee
@@ -0,0 +1,47 @@
+udefine 'chione/base', ['mixer', 'eventmap'], (mixer, EventMap) ->
+  class Base
+    constructor: (@parent, descriptor) ->
+      mixer [@, Base::], new EventMap()
+      
+      if typeof descriptor is 'function'
+        descriptor.call @
+      else
+        for key, value of descriptor
+          if key is 'events'
+            for evKey, evValue of key
+              @on evKey, evValue
+          else
+            # TODO: Don't overwrite prototype methods
+            @[key] = value
+   
+    log: (args...) ->
+      nameArg = (@name || @constructor.name) + ': '
+      
+      console.log.apply console, [].concat.apply(nameArg, args)
+
+udefine 'chione/component', ['chione/base'], (Base) ->
+  class Components extends Base
+udefine 'chione/entity', ['chione/base'], (Base) ->
+  class Entity extends Base
+
+udefine 'chione/game', ['chione/base', 'chione/scene'], (Base, Scene) ->
+  class Game extends Base
+    constructor: (descriptor) ->
+      @scenes = []
+   
+      super null, descriptor
+   
+    scene: (obj) ->
+      console.log Scene
+      scene = if typeof obj is 'object' and obj instanceof Scene
+        obj.parent = @
+        obj
+      else
+        new Scene @, obj
+   
+      @scenes.push scene
+      
+    @run: (sceneName) ->
+
+udefine 'chione/scene', ['chione/base'], (Base) ->
+  class Scene extends Base
--- dist/chione.js
+++ dist/chione.js
@@ -1,68 +1,132 @@
 (function() {
-  var Base, Entity, Game, Scene, _ref, _ref1,
+  (function(namespace) {
+    return (function(modules) {
+      return udefine.configure(function(root) {
+        var m, moduleName, _i, _len;
+        for (_i = 0, _len = modules.length; _i < _len; _i++) {
+          m = modules[_i];
+          moduleName = "" + namespace + "/" + (m.toLowerCase());
+          root[namespace] || (root[namespace] = {});
+          udefine.inject.add(moduleName, {
+            root: root[namespace],
+            name: m
+          });
+        }
+        return null;
+      });
+    })(['Base', 'Entity', 'Game', 'Scene']);
+  })('chione');
+
+}).call(this);
+
+(function() {
+  var __slice = [].slice,
     __hasProp = {}.hasOwnProperty,
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
-  Base = (function() {
-    function Base(parent, descriptor) {
-      var key, value;
-      this.parent = parent;
-      if (typeof descriptor === 'function') {
-        descriptor.call(this);
-      } else {
-        for (key in descriptor) {
-          value = descriptor[key];
-          this[key] = value;
+  udefine('chione/base', ['mixer', 'eventmap'], function(mixer, EventMap) {
+    var Base;
+    return Base = (function() {
+      function Base(parent, descriptor) {
+        var evKey, evValue, key, value;
+        this.parent = parent;
+        mixer([this, Base.prototype], new EventMap());
+        if (typeof descriptor === 'function') {
+          descriptor.call(this);
+        } else {
+          for (key in descriptor) {
+            value = descriptor[key];
+            if (key === 'events') {
+              for (evKey in key) {
+                evValue = key[evKey];
+                this.on(evKey, evValue);
+              }
+            } else {
+              this[key] = value;
+            }
+          }
         }
       }
-    }
 
-    Base.prototype.log = function() {};
+      Base.prototype.log = function() {
+        var args, nameArg;
+        args = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
+        nameArg = (this.name || this.constructor.name) + ': ';
+        return console.log.apply(console, [].concat.apply(nameArg, args));
+      };
 
-    return Base;
+      return Base;
 
-  })();
+    })();
+  });
 
-  Scene = (function(_super) {
-    __extends(Scene, _super);
+  udefine('chione/component', ['chione/base'], function(Base) {
+    var Components, _ref;
+    return Components = (function(_super) {
+      __extends(Components, _super);
 
-    function Scene() {
-      _ref = Scene.__super__.constructor.apply(this, arguments);
-      return _ref;
-    }
+      function Components() {
+        _ref = Components.__super__.constructor.apply(this, arguments);
+        return _ref;
+      }
 
-    return Scene;
+      return Components;
 
-  })(Base);
+    })(Base);
+  });
 
-  Entity = (function(_super) {
-    __extends(Entity, _super);
+  udefine('chione/entity', ['chione/base'], function(Base) {
+    var Entity, _ref;
+    return Entity = (function(_super) {
+      __extends(Entity, _super);
 
-    function Entity() {
-      _ref1 = Entity.__super__.constructor.apply(this, arguments);
-      return _ref1;
-    }
+      function Entity() {
+        _ref = Entity.__super__.constructor.apply(this, arguments);
+        return _ref;
+      }
 
-    return Entity;
+      return Entity;
 
-  })(Base);
+    })(Base);
+  });
 
-  Game = (function(_super) {
-    __extends(Game, _super);
+  udefine('chione/game', ['chione/base', 'chione/scene'], function(Base, Scene) {
+    var Game;
+    return Game = (function(_super) {
+      __extends(Game, _super);
 
-    function Game(descriptor) {
-      this.scenes = [];
-      Game.__super__.constructor.call(this, null, descriptor);
-    }
+      function Game(descriptor) {
+        this.scenes = [];
+        Game.__super__.constructor.call(this, null, descriptor);
+      }
 
-    Game.prototype.scene = function(obj) {
-      var scene;
-      scene = obj instanceof Scene ? (obj.parent = this, obj) : new Scene(this, obj);
-      return this.scenes.push(scene);
-    };
+      Game.prototype.scene = function(obj) {
+        var scene;
+        console.log(Scene);
+        scene = typeof obj === 'object' && obj instanceof Scene ? (obj.parent = this, obj) : new Scene(this, obj);
+        return this.scenes.push(scene);
+      };
 
-    return Game;
+      Game.run = function(sceneName) {};
 
-  })(Base);
+      return Game;
+
+    })(Base);
+  });
+
+  udefine('chione/scene', ['chione/base'], function(Base) {
+    var Scene, _ref;
+    return Scene = (function(_super) {
+      __extends(Scene, _super);
+
+      function Scene() {
+        _ref = Scene.__super__.constructor.apply(this, arguments);
+        return _ref;
+      }
+
+      return Scene;
+
+    })(Base);
+  });
 
 }).call(this);
